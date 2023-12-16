2c39b977c35828ec09fd3496f93dcbab80e8bce5
yovelachou <yovela8888@gmail.com>
2023-12-16 11:38:22 +0800

    1

--- pico_w/lesson9_1.py
+++ pico_w/lesson9_1.py
@@ -2,9 +2,12 @@ import time
 from machine import Pin
  
 led = Pin("LED",Pin.OUT)
- 
+led2 = Pin("LED",Pin.OUT)
 while True:
+    led.high()
+    led2.high() 
     print('run')
-    time.sleep(1)
+    time.sleep(3)
+    led.low()
     print('not run')
-    time.sleep(1)
+    time.sleep(3)
