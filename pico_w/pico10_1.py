ee9c860e36e4c7762fccfd91c655a8eaa6dabecb
yovelachou <yovela8888@gmail.com>
2023-12-16 14:55:02 +0800

    Update pico10_1.py

--- pico_w/pico10_1.py
+++ pico_w/pico10_1.py
@@ -12,13 +12,13 @@ def callback1(t:Timer):
     vol = sensor.read_u16() *( 3.3/65535)
     temperature = 27 -(vol-0.706)/0.001721
     print(f"溫度:{temperature}")
-    delta = time.ticks_diff(time.ticks_ms(), start) # compute time difference
+    delta = time.ticks_diff(time.ticks_ms(), start) # 計算時間差
     print(delta)
-    if temperature >=26 and delta >= 60 * 1000:
+    if temperature >26 and delta >= 60 * 1000: # 每分鐘發警告(若溫度超過26度)
         alert()
-        start = time.ticks_ms()
+        start = time.ticks_ms() #重新計算
         
-start = time.ticks_ms() - 60 * 1000
+start = time.ticks_ms() - 60 * 1000 #起始時間先減60秒，以利第一次超標時發通知
      
 time1 = Timer()
 time1.init(period=1000,callback=callback1)
