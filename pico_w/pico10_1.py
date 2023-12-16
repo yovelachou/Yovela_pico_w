aef2a3d9d752858c3808a21d9818099657b1ecfd
yovelachou <yovela8888@gmail.com>
2023-12-16 14:49:31 +0800

    Update pico10_1.py

--- pico_w/pico10_1.py
+++ pico_w/pico10_1.py
@@ -1,9 +1,24 @@
 from machine import ADC,Timer
+import time
+# Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree.
 
-def callback1(t:Timer):
-    temperature = ADC(4)
-    vol = temperature.read_u16() *( 3.3/65535)
-    print(vol)
+def alert():
+    print("要爆炸了!!!")
     
+def callback1(t:Timer):
+    global start
+    counter = 0
+    sensor = ADC(4)
+    vol = sensor.read_u16() *( 3.3/65535)
+    temperature = 27 -(vol-0.706)/0.001721
+    print(f"溫度:{temperature}")
+    delta = time.ticks_diff(time.ticks_ms(), start) # compute time difference
+    print(delta)
+    if temperature >=26 and delta >= 60 * 1000:
+        alert()
+        start = time.ticks_ms()
+        
+start = time.ticks_ms() - 60 * 1000
+     
 time1 = Timer()
 time1.init(period=1000,callback=callback1)
