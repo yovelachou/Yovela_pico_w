59c208fd6c4371cbfb7fc628f8176d658ff5f645
yovelachou <yovela8888@gmail.com>
2024-01-20 16:22:37 +0800

    1

--- pico_w/pico13_1.py
+++ pico_w/pico13_1.py
@@ -1,4 +1,6 @@
 from machine import Pin
+from tools import connect,reconnect
+import urequests as requests
 import time
 
 
@@ -21,7 +23,23 @@ def btn_detact(btn1):
             led_status = not led_status
             red_led.value(led_status)
             is_press = False
-
+            try:
+                if led_status == True:
+                    get_url = 'https://blynk.cloud/external/api/update?token=d8JG7182JhgWt3s7_vLxWWliHzDmXEKY&V0=1'
+                else:
+                     get_url = 'https://blynk.cloud/external/api/update?token=d8JG7182JhgWt3s7_vLxWWliHzDmXEKY&V0=0'
+                response = requests.get(get_url)       
+            except:
+                reconnect()
+            else:
+                if response.status_code == 200:
+                    print("傳送成功")
+                else:
+                    print("server有錯誤訊息")
+                    print(f'status_code:{response.status_code}')
+                response.close()
+            
+connect()   
 while True:    
     btn_detact(btn)
      
