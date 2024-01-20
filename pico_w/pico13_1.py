3414785024de34bd7cede67e30b5fb3b04ce1bdc
yovelachou <yovela8888@gmail.com>
2024-01-20 15:36:31 +0800

    1

--- pico_w/pico13_1.py
+++ pico_w/pico13_1.py
@@ -1,6 +1,27 @@
 from machine import Pin
+import time
 
 
 red_led = Pin(15,Pin.OUT)
-red_led.value(0)
 btn = Pin(14,mode=Pin.PULL_DOWN)
+is_press = False
+led_status = False
+
+def btn_detact(btn1):
+    global is_press, led_status
+    
+    if btn.value():
+        time.sleep_ms(50)
+        if btn.value():
+           is_press = True
+    elif is_press:
+        time.sleep_ms(50)
+        if btn.value() == False:
+            print('release')
+            led_status = not led_status
+            red_led.value(led_status)
+            is_press = False
+
+while True:    
+    btn_detact(btn)
+     
