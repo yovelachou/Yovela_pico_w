e8b01dad34fc7a6ee6271ae2260020bcc74085db
yovelachou <yovela8888@gmail.com>
2024-01-27 14:40:08 +0800

    Update pico14_1.py

--- pico_w/pico14_1.py
+++ pico_w/pico14_1.py
@@ -8,10 +8,12 @@ def fun10(t:Timer | None = None):
     led.toggle()
    
 def fun500ms(t:Timer):
-    print(light.read_u16())
-    
+    print(f'Light:{light.read_u16()}')
+    print(f'Read:{vr.read_u16()}')
 led = Pin(15,Pin.OUT)
 light = ADC(Pin(28))
+vr = ADC(Pin(27))
+
 timer10 = Timer(period=10000, mode=Timer.PERIODIC, callback=fun10)
 timer100ms = Timer(period=500, mode=Timer.PERIODIC, callback=fun500ms)
 
