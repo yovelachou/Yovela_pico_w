f408f8913e6471afcf7d57a3846fa1bc853bd055
yovelachou <42538114+yovelachou@users.noreply.github.com>
2024-02-03 07:02:42 +0000

    1

--- GUI/main.py
+++ GUI/main.py
@@ -10,11 +10,11 @@ st.header('Pico :blue[cool] :sunglasses:', divider='red')
 # st.divider()
 
 
-url = 'https://blynk.cloud/external/api/get?token=d8JG7182JhgWt3s7_vLxWWliHzDmXEKY&v0&V1'
+url = 'https://blynk.cloud/external/api/get?token=d8JG7182JhgWt3s7_vLxWWliHzDmXEKY&V0&V1'
 response = requests.request("GET",url)
 if response.status_code ==200:
     all_data = response.json()
-    st.info(f'光線:{all_data["v0"]}')
+    st.info(f'光線:{all_data["V0"]}')
     st.warning(f'可變電阻:{all_data["V1"]}')
 else:
     st.write("連線失敗，請稍後再試")
