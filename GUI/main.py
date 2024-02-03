c506e255dd2cc0079dde9e9ec35efeea48a691fc
yovelachou <42538114+yovelachou@users.noreply.github.com>
2024-02-03 07:36:55 +0000

    1

--- GUI/main.py
+++ GUI/main.py
@@ -2,15 +2,18 @@ import streamlit as st
 import pandas as pd
 import requests
 from streamlit_autorefresh import st_autorefresh
+import dotenv
+
+dotenv.load()
 
 st_autorefresh(interval=5000)
 
 st.title('Yovela的Pico_W專案')
 st.header('Pico :blue[cool] :sunglasses:', divider='red')
 # st.divider()
+#d8JG7182JhgWt3s7_vLxWWliHzDmXEKY
 
-
-url = 'https://blynk.cloud/external/api/get?token=d8JG7182JhgWt3s7_vLxWWliHzDmXEKY&V0&V1'
+url = 'https://blynk.cloud/external/api/get?token={os.environ}&V0&V1'
 response = requests.request("GET",url)
 if response.status_code ==200:
     all_data = response.json()
