e1cc241a5f86577866ed5aa5748b16c7bb36ed7f
yovelachou <42538114+yovelachou@users.noreply.github.com>
2024-02-03 06:34:16 +0000

    1

--- GUI/main.py
+++ GUI/main.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import pandas as pd
+import requests
+
+
 
 st.title('Pico_W專案123')
-st.header('Pico :blue[cool] :sunglasses:', divider='red')
+st.header('Pico :blue[cool] :sunglasses:', divider='red'
 # st.divider()
 
 
+url = 'https://blynk.cloud/external/api/get?token=d8JG7182JhgWt3s7_vLxWWliHzDmXEKY&v0&V1'
+response = requests.request("GET",url)
+if response.status_code ==200:
+    all_data = response.json()
+    st.info(f'光線:{all_data["V0"]}')
+    st.warning(f'可變電阻:{all_data["V1"]}')
+else:
+    st.write("連線失敗，請稍後再試")
